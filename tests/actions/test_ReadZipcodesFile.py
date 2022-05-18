91a2228414e014c168d87ef33ea7fe9cc0f9d15f
didac <didac.elgueta@gmail.com>
2022-05-18 17:30:59 +0200

    add coverage and coverage-report to Makefile

--- tests/actions/test_ReadZipcodesFile.py
+++ tests/actions/test_ReadZipcodesFile.py
@@ -6,10 +6,9 @@ from wg_be_exam.actions.readZipcodesFile import ReadZipcodesFile
 
 class test_ReadZipcodesFile(TestCase):
     def test_read_zipcodes_file(self):
-        return_value = pandas.DataFrame({'zipcodes': [1,2,3], 'risk_factor': ['A', 'B', 'A']})
+        return_value = pandas.DataFrame({'zipcodes': ['1','1111 - 1111','3'], 'risk_factor': ['A', 'B', 'A']})
+        return_value.set_index('zipcodes', inplace=True)
         with patch.object(pandas, 'read_csv', return_value=return_value) as mock_read_csv:
             output = ReadZipcodesFile.handle('fake.file')
 
-            mocked_output = return_value.set_index('zipcodes')
-
-            self.assertEqual(len(mocked_output.index.values), len(output.index.values))
+            self.assertEqual(len(return_value.index.values), len(output.index.values))
